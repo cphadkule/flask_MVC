ed6a6843500e7f6cf85566c59cb134b218a0a9da
cphadkule <cphadkule@gmail.com>
2018-11-06 17:29:52 +0530

    todo using flask. GET POST PUT DELETE URL AUTH

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 tasks = [
     {
@@ -16,11 +18,11 @@ tasks = [
         'done': False
     }
 ]
-
+"""
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
-
+"""
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -72,6 +74,30 @@ def delete_task(task_id):
     tasks.remove(task[0])
     return jsonify({'result': True})
 
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+@auth.get_password
+def get_password(username):
+    if username == 'cphadkule':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 if __name__ == '__main__':
     app.run(debug=True)
